e791bf73b3e4005cd630f6853bbf18dba77078e6
Muchamad Rofii <mhdrf170756@gmail.com>
2025-07-19 12:42:34 +0700

    Add files via upload

---
+++ Pertemuan 3/Praktikum/just a tet.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Hewan{
+	private:
+		string nama;
+		string spesies;
+	
+	public:
+		void setNama(string n){
+			nama = n;
+		}
+		
+		void setSpesies(string s){
+			spesies = s;
+		}
+		
+		string getNama(){
+			return nama;
+		}
+		
+		string getSpesies(){
+			return spesies;
+		}
+};
+
+int main(){
+	Hewan hewan;
+	hewan.setNama();
+	hewan.setSpesies();
+	
+	getlien
+	
+	cout<<"Nama : "<<hewan.getNama()<<endl;
+	cin >> hewan.setNama()
+	cout<<"Spesies : "<<hewan.getSpesies()<<endl;
+	cin >> set
+	return 0;
+}
